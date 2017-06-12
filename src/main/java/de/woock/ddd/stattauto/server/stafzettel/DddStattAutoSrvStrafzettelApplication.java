cd02dec66daf4abce87ee550896d03ad7fb60ba6
Hermann Woock <hermann.woock@oose.de>
2017-06-12 17:32:54 +0200

    initial commit

---
+++ src/main/java/de/woock/ddd/stattauto/server/stafzettel/DddStattAutoSrvStrafzettelApplication.java
@@ -0,0 +1,12 @@
+package de.woock.ddd.stattauto.server.stafzettel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddStattAutoSrvStrafzettelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddStattAutoSrvStrafzettelApplication.class, args);
+	}
+}
---
+++ src/test/java/de/woock/ddd/stattauto/server/stafzettel/DddStattAutoSrvStrafzettelApplicationTests.java
@@ -0,0 +1,16 @@
+package de.woock.ddd.stattauto.server.stafzettel;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DddStattAutoSrvStrafzettelApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
